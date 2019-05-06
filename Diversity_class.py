258b1788955ed1a8a69e8ba22e31a32087b72425
PKatarina <45435942+PKatarina@users.noreply.github.com>
2019-05-06 16:08:22 +0200

    Add files via upload

--- Diversity_class.py
+++ Diversity_class.py
@@ -1,7 +1,7 @@
 import Biom_table
 import math
 
-class Diversity:
+class Diversity(Biom_table.Biom_table):
 
     """
     To initialize class input a biom_table class
@@ -9,39 +9,41 @@ class Diversity:
 
     
     def __init__(self,a_biom_table):
-        self.abundance=a_biom_table.tax_abundance('p')
+        Biom_table.Biom_table.__init__(self,a_biom_table)
+        #self.abundance=a_biom_table.tax_abundance('p')
+        self.abundance=self.tax_abundance()
 
     def __str__(self):
         return ("This is a diversity class")
 
-    def richness(self):
+    def richness(self,taxonomy='s'):
         """
         Returns the number of different species in the sample
         """
-        return len(self.abundance)
+        return len(self.tax_abundance(taxonomy))
 
-    def shannon_index(self):
+    def shannon_index(self,taxonomy='s'):
         """
         Calculates shannon index (H) of sample
         H=-SUM(pi*ln(pi))
         """
         H=0
-        for data in self.abundance:
+        for data in self.tax_abundance(taxonomy):
             H+=data[1]/100*math.log(data[1]/100)
         return round(-H,2)
 
-    def simpson_index(self):
+    def simpson_index(self,taxonomy='s'):
         """
         Calculates simpson index (D) of sample
         D= 1-SUM(pi^2)
         """
         D=0
-        for data in self.abundance:
+        for data in self.tax_abundance(taxonomy):
             D+=math.pow(data[1]/100,2)
         
         return round(1-D,2)
 
-    def jaccard_index(self,abundance_2):
+    def jaccard_index(self,abundance_2,taxonomy='s'):
         """
         Takes a list of species abundances for another sample and calculates the jaccard index
         for both samples. Jaccard index represents the similarity of species and their abundance
@@ -54,14 +56,15 @@ class Diversity:
         U=0
         V=0
 
-        for species_1 in self.abundance:
+        for species_1 in self.tax_abundance(taxonomy):
             for species_2 in abundance_2:
                 if species_1[0]==species_2[0]:
                     U+=species_1[1]/100
                     V+=species_2[1]/100
         if U==0 or V==0:
-            print("There are no shared species from these two samples")
-            return 
+            raise Exception("There are no shared species from these two samples")
+            # print("There are no shared species from these two samples")
+            # return 
         
         return round((U*V)/(U+V-U*V),2)
 
@@ -70,10 +73,13 @@ class Diversity:
 a_name= "example1_metaphlan.txt"
 
 
-mytable = Diversity(Biom_table.Biom_table(a_name))
+#mytable = Diversity(Biom_table.Biom_table(a_name)
+mytable = Diversity(a_name)
 
 print (mytable, "Richness: ",mytable.richness(), "Shannon index: ", mytable.shannon_index(), "Simpson index: ", mytable.simpson_index())
 
 sample2_abundance=[("Bacteroidetes", 50.00),("Proteobacteria",20.00),("Firmicutes",20.00),("Dummy",10.00)]
 
-print("Jaccard index: ",mytable.jaccard_index(sample2_abundance))
+print("Jaccard index: ",mytable.jaccard_index(sample2_abundance, 'p'))
+
+print (mytable.tax_abundance('p'))
