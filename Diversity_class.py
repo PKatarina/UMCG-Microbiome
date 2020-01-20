c1512b3944c7736b36062bb4401af8ae1e6aebe8
PKatarina <45435942+PKatarina@users.noreply.github.com>
2020-01-20 10:23:02 +0100

    Update Diversity_class.py

--- Diversity_class.py
+++ Diversity_class.py
@@ -1,85 +1,82 @@
-import Biom_table
+import BiomTable_class
 import math
 
-class Diversity(Biom_table.Biom_table):
 
+class Diversity(BiomTable_class.BiomTable):
     """
-    To initialize class input a biom_table class
+    To initialize class input filename of a table
     """
 
-    
-    def __init__(self,a_biom_table):
-        Biom_table.Biom_table.__init__(self,a_biom_table)
-        #self.abundance=a_biom_table.tax_abundance('p')
-        self.abundance=self.tax_abundance()
+    def __init__(self, a_biom_table):
+        BiomTable_class.BiomTable.__init__(self, a_biom_table)
+        self.abundance = self.tax_abundance()
 
     def __str__(self):
-        return ("This is a diversity class")
+        return "This is a diversity class"
 
-    def richness(self,taxonomy='s'):
+    def richness(self, taxonomy='s'):
         """
         Returns the number of different species in the sample
         """
         return len(self.tax_abundance(taxonomy))
 
-    def shannon_index(self,taxonomy='s'):
+    def shannon_index(self, taxonomy='s'):
         """
         Calculates shannon index (H) of sample
         H=-SUM(pi*ln(pi))
         """
-        H=0
+        h = 0
         for data in self.tax_abundance(taxonomy):
-            H+=data[1]/100*math.log(data[1]/100)
-        return round(-H,2)
+            h += data[1] / 100 * math.log(data[1] / 100)
+        return round(-h, 2)
 
-    def simpson_index(self,taxonomy='s'):
+    def simpson_index(self, taxonomy='s'):
         """
-        Calculates simpson index (D) of sample
-        D= 1-SUM(pi^2)
+        Calculates simpson index (d) of sample
+        d= 1-SUM(pi^2)
         """
-        D=0
+        d = 0
         for data in self.tax_abundance(taxonomy):
-            D+=math.pow(data[1]/100,2)
-        
-        return round(1-D,2)
+            d += math.pow(data[1] / 100, 2)
 
-    def jaccard_index(self,abundance_2,taxonomy='s'):
+        return round(1 - d, 2)
+
+    def jaccard_index(self, abundance_2, taxonomy='s'):
         """
         Takes a list of species abundances for another sample and calculates the jaccard index
         for both samples. Jaccard index represents the similarity of species and their abundance
          present in two samples. 
-         J = U*V/(U+V-UV)
-         U = sum of abundances of shared species from sample 1
-         V = sum of abundances of shared species from sample 2
+         J = u*v/(u+v-uv)
+         u = sum of abundances of shared species from sample 1
+         v = sum of abundances of shared species from sample 2
         """
 
-        U=0
-        V=0
+        u = 0
+        v = 0
 
         for species_1 in self.tax_abundance(taxonomy):
             for species_2 in abundance_2:
-                if species_1[0]==species_2[0]:
-                    U+=species_1[1]/100
-                    V+=species_2[1]/100
-        if U==0 or V==0:
+                if species_1[0] == species_2[0]:
+                    u += species_1[1] / 100
+                    v += species_2[1] / 100
+        if u == 0 or v == 0:
             raise Exception("There are no shared species from these two samples")
-            # print("There are no shared species from these two samples")
-            # return 
-        
-        return round((U*V)/(U+V-U*V),2)
 
+        return round((u * v) / (u + v - u * v), 2)
 
+    def top_x_abundance(self, list_lenght, taxonomy = 's'):
+        """
+        list_lenght: Lenght of the list
+        taxonomy: taxonomy level
+        Returns a list of input lenght of top taxonomy level sorted by abundance
 
-a_name= "example1_metaphlan.txt"
+        """
+        counter = 0
+        a_list = []
+        for species in self.tax_abundance(taxonomy):
+            a_list.append(species)
+            counter += 1
+            if counter == list_lenght:
+                break
 
-
-#mytable = Diversity(Biom_table.Biom_table(a_name)
-mytable = Diversity(a_name)
-
-print (mytable, "Richness: ",mytable.richness(), "Shannon index: ", mytable.shannon_index(), "Simpson index: ", mytable.simpson_index())
-
-sample2_abundance=[("Bacteroidetes", 50.00),("Proteobacteria",20.00),("Firmicutes",20.00),("Dummy",10.00)]
-
-print("Jaccard index: ",mytable.jaccard_index(sample2_abundance, 'p'))
-
-print (mytable.tax_abundance('p'))
+        return a_list
