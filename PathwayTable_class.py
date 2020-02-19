69c365f39e2ff26f1899831c23be63d4ad68d704
PKatarina <45435942+PKatarina@users.noreply.github.com>
2020-02-19 17:00:09 +0100

    Update PathwayTable_class.py
    Deleting mergesort because file is sorted after filtration

--- PathwayTable_class.py
+++ PathwayTable_class.py
@@ -1,5 +1,6 @@
 import csv
 
+
 class PathwayTable:
 
     """
@@ -17,9 +18,6 @@ class PathwayTable:
                     self.rows.append(line)
                     self.total_abundance += float(line[1])
 
-        for lines in self.rows:
-            lines.append(round(float(line[1]) / self.total_abundance, 6))
-
         self.UNSORTED_LIST = list(self.rows)
 
     def __str__(self):
@@ -27,48 +25,6 @@ class PathwayTable:
         Returns a string representation of the table name
         """
         return str(self.table)
-
-    @staticmethod
-    def merge(list1, list2):
-        """
-        Merge two sorted lists.
-
-        Returns a new sorted list containing those elements that are in
-        either list1 or list2.
-
-        This function is iterative.
-
-        Helper function for merge_sort
-        """
-        new_list = []
-        while list1 != [] or list2 != []:
-            if not list1:
-                new_list.extend(list2)
-                return new_list
-            if not list2:
-                new_list.extend(list1)
-                return new_list
-            elif list1[0][1] <= list2[0][1]:
-                new_list.append(list1.pop(0))
-            else:
-                new_list.append(list2.pop(0))
-
-        return new_list
-
-    def merge_sort(self, list1):
-        """
-        Sort the elements of list1.
-
-        Return a new sorted list with the same elements as list1.
-
-        This function is recursive.
-        """
-
-        # base case
-        if len(list1) <= 1:
-            return list1
-        else:
-            return self.merge(self.merge_sort(list1[0:int(len(list1)/2)]), self.merge_sort(list1[int(len(list1) / 2):]))
 
     def get_total_abundance(self):
         """
@@ -83,8 +39,8 @@ class PathwayTable:
         """
 
         top_pathways = []
-        for idx in range(1, list_lenght+1):
-            top_pathways.append(self.merge_sort(self.UNSORTED_LIST)[-idx])
+        for idx in range(list_lenght):
+            top_pathways.append(self.rows[idx])
 
         return top_pathways
 
